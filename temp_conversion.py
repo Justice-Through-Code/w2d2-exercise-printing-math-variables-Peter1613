fa973c84026f7bb54410d6a6c41b894e18ca0560
Peter <reachpjok@gmail.com>
2023-09-11 14:08:00 -0400

    add working temp_conversion.py

--- temp_conversion.py
+++ temp_conversion.py
@@ -1,63 +1,57 @@
-
-# def convert_100_to_celsius  
-
+#1
+def convert_100_to_celsius():
+    celsius_100 = (100-32)*5/9
+    print(celsius_100)
+    print("float")
+    
+    #Peter's reply: I know it is a float because of  the decimal
+    
     # Convert a temperature of 100 degrees fahrenheit to celsius
-convert_100_to_celsius = ((100-32)*5/9)
-    
-    # Save this to a variable called celsius_100, and use print() to print out the value   
-celsius_100 = convert_100_to_celsius
-print("100 degrees fahreinhet =",celsius_100, "celsius.")
+    # Save this to a variable called celsius_100, and use print() to print out the value
     # Is the resulting temperature you get an integer or float?
-    # Peter's reply: Float 37.77777
-    
     # Print 'float' if it is a float or 'int' if it is an int
-print(type(celsius_100))
-    
     # How do you know? Write a comment below your code explaining how you know which it is
-    #Peter's reply: I knew it was a float for two reasons: 1. the decimal    2. the type() function.
-
+     
 #convert_100_to_celsius()
 
-# def convert_0_to_celsius = ((0-32)*5/9)
+#2
+def convert_0_to_celsius():
+    celsius_0 = ((0-32)*5/9)
+    print(celsius_0)
 
     # Convert a temperature of 0 degrees fahrenheit to celsius
-convert_0_to_celsius = ((0-32)*5/9)
-
     # Save this to a variable called celsius_0, and use print() to print out the value
-celsius_0 = convert_0_to_celsius
-print("0 degrees fahrenheit =",celsius_0, "celsius.")
 
 #convert_0_to_celsius()
 
-# def convert_34_2_to_celsius():
-
+#3
+def convert_34_2_to_celsius():
+    print((34.2-32)*5/9)
+    
     # Convert a temperature of 34.2 degrees fahrenheit to celsius
     # Do this one all in one print statement without saving any variables
-convert_34_2_to_celsius = ((34.2-32)*5/9)
-print("34.2 degrees fahrenheit =", convert_34_2_to_celsius, "celsius.")
         
 #convert_34_2_to_celsius()
 
+#4
 # Now, can you convert back?
 convert_back_to_fahrenheit = ((1.2222222222222239*9/5)+32)
-print("1.2222222222222239 celsius =", convert_back_to_fahrenheit,"fahrenheit." )
+print(convert_back_to_fahrenheit)
 
-# def convert_5_to_fahrenheit():
-
-    # Convert a temperature of 5 degrees celsius to fahrenheit and print it out
-convert_5_to_fahrenheit = ((5*9/5)+32)
-print("5 degrees celsius =", convert_5_to_fahrenheit,"fahrenheit.")
+#5
+def convert_5_to_fahrenheit():
+    print((5*9/5)+32)
+    
+# Convert a temperature of 5 degrees celsius to fahrenheit and print it out
 
 #convert_5_to_fahrenheit()
 
-# def hotter_temp():
+#6
+def hotter_temp():
+    convert = ((30.2*9/5)+32)
+    print("30.2 degrees celsius")
 
     # What is hotter, a temperature of 30.2 degrees celsius, or a temperature of 85.1 degrees fahrenheit?
     # Print out the hotter temp: '30.2 degrees celsius' or '85.1 degrees fahrenheit', respectively
-
-convert_30_2_celsius_to_fahrenheit = ((30.2*9/5)+32)
-print("30.2 degrees celsius =", convert_30_2_celsius_to_fahrenheit, "fahrenheit.")
-hotter_temp = ("30.2 degrees celsius")
-print(hotter_temp,"is the hotter temp." )
 
 #hotter_temp()
